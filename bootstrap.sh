ddc40fb69de162588c166f48926c0834e35d0351
Tyler Hovanec <tbhovanec>
2020-03-19 18:24:22 -0700

    Switch to VBoxService for startup/shutdown.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,4 @@ apt-get install -y docker-compose
 
 cd /vagrant_data/
 
-# Diabled for Docker Volumes - this would work if we created the files outside of the vagrant share where they can actually have 600 permissions.
-# Set up Traefik files: https://stackoverflow.com/a/52524107
-# touch letsencrypt/acme.json
-# chmod 600 letsencrypt/acme.json
-
 docker-compose up -d
